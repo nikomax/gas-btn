09a997f9cc35061290e423186108e5bf985101ef
max <maks@coderiver.com.ua>
2022-01-05 22:28:31 +0200

    upd

---
+++ app.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector('.js-circle')
+
+// window.onload = function () {
+// 	gsap.registerPlugin(ScrollTrigger);
+// 	gsap.timeline({
+// 		scrollTrigger: {
+// 			trigger:".wrapper",
+// 			start: "top top",
+// 			end: "bottom top",
+// 			pin: true,
+// 			markers: true,
+// 			scrub: true,
+// 			onEnter: () => btn.classList.add('is-active'),
+// 			onLeave: () => btn.classList.remove('is-active'),
+// 			onEnterBack: () => btn.classList.add('is-active'),
+// 			onLeaveBack: () => btn.classList.remove('is-active'),
+// 		}
+// 	})
+// }
