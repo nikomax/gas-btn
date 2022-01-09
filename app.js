2f55eab2020caba4b5e9d1390fa033753efc7675
max <maks@coderiver.com.ua>
2022-01-09 15:32:19 +0200

    add load lisener

--- app.js
+++ app.js
@@ -8,41 +8,44 @@ function setTheme(index) {
 	page.classList = themes[index]
 }
 
-const observerCircle = new IntersectionObserver((entries) => {
-		entries.forEach(entry => {
-			if (entry.isIntersecting) {
-				section.classList.add('is-active');
-			} else {
-				section.classList.remove('is-active');
-			}
-		})
-	}
-)
-
-observerCircle.observe(section);
-
-let directionTop = false
-let lastScroll = 0;
-const observerText = new IntersectionObserver((entries) => {
-		let currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
-		if (currentScroll > 0 && lastScroll <= currentScroll){
-			lastScroll = currentScroll;
-			directionTop = false;
-		} else {
-			lastScroll = currentScroll;
-			directionTop = true;
+window.onload = () => {
+	const observerCircle = new IntersectionObserver((entries) => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					section.classList.add('is-active');
+				} else {
+					section.classList.remove('is-active');
+				}
+			})
 		}
-		entries.forEach(entry => {
-			if (entry.isIntersecting) {
-				circle.classList.add('is-gas');
-				section.classList.add('theme-changed');
-			} else if (directionTop) {
-				circle.classList.remove('is-gas');
-				section.classList.remove('theme-changed');
-			}
-		})
-	},
-	{threshold: 0.2}
-)
+	)
 
-observerText.observe(gasContent);
+	observerCircle.observe(section);
+
+	let directionTop = false
+	let lastScroll = 0;
+	const observerText = new IntersectionObserver((entries) => {
+			let currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
+			if (currentScroll > 0 && lastScroll <= currentScroll){
+				lastScroll = currentScroll;
+				directionTop = false;
+			} else {
+				lastScroll = currentScroll;
+				directionTop = true;
+			}
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					circle.classList.add('is-gas');
+					section.classList.add('theme-changed');
+				} else if (directionTop) {
+					circle.classList.remove('is-gas');
+					section.classList.remove('theme-changed');
+				}
+			})
+		},
+		{threshold: 0.2}
+	)
+
+	observerText.observe(gasContent);
+
+}
