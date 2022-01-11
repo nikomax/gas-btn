8376f2fd1f63af0c6bb712b0f6a977e6261b87f0
max <maks@coderiver.com.ua>
2022-01-11 17:04:35 +0200

    change trigger offset

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const setClasses = () => {
 		section.classList.remove('is-active');
 	}
 
-	if (gasContent.getBoundingClientRect().top - window.innerHeight < 0) {
+	if (gasContent.getBoundingClientRect().top - window.innerHeight/2 < 0) {
 		circle.classList.add('is-gas');
 		section.classList.add('theme-changed');
 	} else if (directionTop) {
@@ -29,52 +29,9 @@ const setClasses = () => {
 	}
 }
 
-
-
 window.onload = () => {
 	setClasses();
 	window.addEventListener('scroll', () => {
 		setClasses()
 	})
-	// const observerCircle = new IntersectionObserver((entries) => {
-	// 		entries.forEach(entry => {
-	// 			if (entry.isIntersecting) {
-	// 				section.classList.add('is-active');
-	// 			} else {
-	// 				section.classList.remove('is-active');
-	// 			}
-	// 		})
-	// 	}
-	// )
-	//
-	// observerCircle.observe(section);
-
-	// let directionTop = false
-	// let lastScroll = 0;
-	// const observerText = new IntersectionObserver((entries) => {
-	// 		let currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
-	// 		if (currentScroll > 0 && lastScroll <= currentScroll){
-	// 			lastScroll = currentScroll;
-	// 			directionTop = false;
-	// 		} else {
-	// 			lastScroll = currentScroll;
-	// 			directionTop = true;
-	// 		}
-	// 		entries.forEach(entry => {
-	// 			if (entry.isIntersecting) {
-	// 				circle.classList.add('is-gas');
-	// 				section.classList.add('theme-changed');
-	// 			} else if (directionTop) {
-	// 				circle.classList.remove('is-gas');
-	// 				section.classList.remove('theme-changed');
-	// 			}
-	// 		})
-	// 	},
-	// 	{
-	// 		threshold: 0.1
-	// 	}
-	// )
-
-	// observerText.observe(gasContent);
-
 }
